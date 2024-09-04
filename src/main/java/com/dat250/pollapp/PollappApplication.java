8a263ba4fe44ad288378f419fc924d2470913a6c
Mikael Færøvik <mikael.farovik@gmail.com>
2024-09-04 10:58:39 +0200

    initial commit

---
+++ src/main/java/com/dat250/pollapp/PollappApplication.java
@@ -0,0 +1,13 @@
+package com.dat250.pollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dat250/pollapp/PollappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dat250.pollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
